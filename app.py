8462a8374b90e553aa0e1fe8e262dde897962a45
Way-dant <vedant.sarmaalkar21@gmail.com>
2026-03-30 21:46:14 +0530

    Updated notebook, app logic, and UI

--- app.py
+++ app.py
@@ -1,21 +1,34 @@
 from flask import Flask, render_template, request
 import pickle
+import re
 
 app = Flask(__name__)
 
 model = pickle.load(open("model.pkl","rb"))
 vectorizer = pickle.load(open("vectorizer.pkl","rb"))
 
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r'\W',' ',text)
+    text = re.sub(r'\s+',' ',text)
+    return text
+
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", prediction=None)
+
 
 @app.route("/predict", methods=["POST"])
 def predict():
 
+    title = request.form["title"]
     news = request.form["news"]
 
-    vector = vectorizer.transform([news])
+    content = title + " " + news
+    content = clean_text(content)
+
+    vector = vectorizer.transform([content])
 
     prediction = model.predict(vector)
 
@@ -24,7 +37,13 @@ def predict():
     else:
         result = "Real News"
 
-    return render_template("index.html", prediction=result)
+    return render_template(
+        "index.html",
+        prediction=result,
+        title=title,
+        news=news
+    )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
